73ef45748438454be7cf6e12c684820a6726ba89
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 23:01:55 -0800

    meaningful parameter names

--- base/SpacePlace.java
+++ base/SpacePlace.java
@@ -1,58 +1,59 @@
 package base;
 
 /**
+ * Represents a location in space.
+ * 
+ * Manages the coordinates and angles of a point in space.
+ * 
  * @author Kevan Buckley, maintained by __student
  * @version 2.0, 2014
  */
-
 public class SpacePlace {
-	private int xOrg;
-	private int yOrg;
-	private double theta;
-	private double phi;
+    private int xCoordinate;
+    private int yCoordinate;
+    private double theta;
+    private double phi;
 
-	public SpacePlace() {
-        this(0, 0, 0.0, 0.0);
+    public SpacePlace() {
+        xCoordinate = 0;
+        yCoordinate = 0;
     }
 
-
-	public SpacePlace(int xOrg, int yOrg, double theta, double phi) {
-        this.xOrg = xOrg;
-        this.yOrg = yOrg;
+    public SpacePlace(double theta, double phi) {
+        this();
         this.theta = theta;
         this.phi = phi;
     }
 
-	public int getxOrg() {
-		return xOrg;
-	}
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
 
-	public void setxOrg(int xOrg) {
-		this.xOrg = xOrg;
-	}
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
 
-	public int getyOrg() {
-		return yOrg;
-	}
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
 
-	public void setyOrg(int yOrg) {
-		this.yOrg = yOrg;
-	}
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
 
-	public double getTheta() {
-		return theta;
-	}
+    public double getTheta() {
+        return theta;
+    }
 
-	public void setTheta(double theta) {
-		this.theta = theta;
-	}
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
 
-	public double getPhi() {
-		return phi;
-	}
+    public double getPhi() {
+        return phi;
+    }
 
-	public void setPhi(double phi) {
-		this.phi = phi;
-	}
-
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
 }
