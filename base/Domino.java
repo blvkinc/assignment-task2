93ea0e4847b50811d92e34450d655424ae19b788
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 22:37:13 -0800

    method extraction and adding new comments

--- base/Domino.java
+++ base/Domino.java
@@ -1,12 +1,16 @@
 package base;
 
 /**
+ * Represents a domino tile with two numbers.
+ * 
+ * This class encapsulates the behavior and properties of a domino tile.
+ * It includes methods for placement, inversion, comparison, and string representation.
+ * 
  * @author Kevan Buckley, maintained by __student
  * @version 2.0, 2014
  */
-
 public class Domino implements Comparable<Domino> {
-	private int high;
+    private int high;
     private int low;
     private int hx;
     private int hy;
@@ -14,66 +18,85 @@ public class Domino implements Comparable<Domino> {
     private int ly;
     private boolean placed = false;
 
-	public Domino(int high, int low) {
-		super();
-		this.high = high;
-		this.low = low;
-	}
+    /**
+     * Constructor for creating a domino with given high and low values.
+     * 
+     * @param high The higher value of the domino
+     * @param low  The lower value of the domino
+     */
+    public Domino(int high, int low) {
+        this.high = high;
+        this.low = low;
+    }
 
-	public void place(int hx, int hy, int lx, int ly) {
-		this.hx = hx;
-		this.hy = hy;
-		this.lx = lx;
-		this.ly = ly;
-		placed = true;
-	}
+    /**
+     * Places the domino on the board at specified coordinates.
+     * 
+     * @param higherX x-coordinate for the higher value
+     * @param higherY y-coordinate for the higher value
+     * @param lowerX  x-coordinate for the lower value
+     * @param lowerY  y-coordinate for the lower value
+     */
+    public void place(int higherX, int higherY, int lowerX, int lowerY) {
+        this.hx = higherX;
+        this.hy = higherY;
+        this.lx = lowerX;
+        this.ly = lowerY;
+        placed = true;
+    }
 
-	public String toString() {
-		StringBuffer result = new StringBuffer();
-		result.append("[");
-		result.append(Integer.toString(high));
-		result.append(Integer.toString(low));
-		result.append("]");
-		if (!placed) {
-			result.append("unplaced");
-		} else {
-			result.append("(");
-			result.append(Integer.toString(hx + 1));
-			result.append(",");
-			result.append(Integer.toString(hy + 1));
-			result.append(")");
-			result.append("(");
-			result.append(Integer.toString(lx + 1));
-			result.append(",");
-			result.append(Integer.toString(ly + 1));
-			result.append(")");
-		}
-		return result.toString();
-	}
+    /**
+     * Returns a string representation of the domino.
+     * 
+     * @return String representation of the domino
+     */
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+        result.append(high);
+        result.append(low);
+        result.append("]");
 
-	/** turn the domino around 180 degrees clockwise */
+        if (!placed) {
+            result.append(" unplaced");
+        } else {
+            result.append("(");
+            result.append(hx + 1);
+            result.append(",");
+            result.append(hy + 1);
+            result.append(")");
+            result.append("(");
+            result.append(lx + 1);
+            result.append(",");
+            result.append(ly + 1);
+            result.append(")");
+        }
+        return result.toString();
+    }
 
-	public void invert() {
-		int tx = hx;
-		hx = lx;
-		lx = tx;
+    /**
+     * Inverts the domino by rotating it 180 degrees clockwise.
+     */
+    public void invert() {
+        int tempX = hx;
+        hx = lx;
+        lx = tempX;
 
-		int ty = hy;
-		hy = ly;
-		ly = ty;
-	}
+        int tempY = hy;
+        hy = ly;
+        ly = tempY;
+    }
 
-	public boolean ishl() {
-		return hy == ly;
-	}
+    /**
+     * Checks if the domino is placed horizontally.
+     * 
+     * @return True if the domino is placed horizontally, otherwise false
+     */
+    public boolean isHorizontal() {
+        return hy == ly;
+    }
 
-	public int compareTo(Domino arg0) {
-		if (this.high < arg0.high) {
-			return 1;
-		}
-		return this.low - arg0.low;
-	}
-
+    // Getter and Setter methods for private fields
 
     public int getHigh() {
         return high;
@@ -129,5 +152,19 @@ public class Domino implements Comparable<Domino> {
 
     public void setPlaced(boolean placed) {
         this.placed = placed;
+    }
+
+    /**
+     * Compares this domino to another based on their high and low values.
+     * 
+     * @param other The other domino to compare with
+     * @return 1 if this domino's high value is less than the other, 
+     *         otherwise the comparison of their low values
+     */
+    public int compareTo(Domino other) {
+        if (this.high < other.high) {
+            return 1;
+        }
+        return this.low - other.low;
     }
 }
