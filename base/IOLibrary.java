87ac594a36b071e0c0c8c7a8dd02f9286ba69c8a
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 22:41:43 -0800

    method extraction and exception handling

--- base/IOLibrary.java
+++ base/IOLibrary.java
@@ -25,16 +25,20 @@ public final class IOLibrary {
     }
 
 	public static InetAddress getIPAddress() {
-		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-		do {
-			try {
-				String[] chunks = r.readLine().split("\\.");
-				byte[] data = { Byte.parseByte(chunks[0]), Byte.parseByte(chunks[1]), Byte.parseByte(chunks[2]),
-						Byte.parseByte(chunks[3]) };
-				return Inet4Address.getByAddress(data);
-			} catch (Exception e) {
-			}
-		} while (true);
+        do {
+            try {
+                String[] chunks = reader.readLine().split("\\.");
+                byte[] data = {
+                    Byte.parseByte(chunks[0]),
+                    Byte.parseByte(chunks[1]),
+                    Byte.parseByte(chunks[2]),
+                    Byte.parseByte(chunks[3])
+                };
+                return Inet4Address.getByAddress(data);
+            } catch (IOException | NumberFormatException e) {
+                // Handle or log the exception
+            }
+        } while (true);
 	}
 
 }
