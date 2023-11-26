0f93793ce4d5bfa13f18c6eb747d83b2084b64aa
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 22:58:29 -0800

    comments added to describe methods

--- base/PictureFrame.java
+++ base/PictureFrame.java
@@ -8,50 +8,55 @@ public class PictureFrame {
     private Main master = null;
     private DominoPanel dp;
 
+    // Inner class representing the panel to display the grid
     class DominoPanel extends JPanel {
         private static final long serialVersionUID = 4190229282411119364L;
 
+        // Overrides the paintComponent method to paint the panel
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             drawBackground(g);
             Location l = new Location(1, 2);
-            if (master != null) {
-                if (master.mode == 1 || master.mode == 0) {
-                    l.drawGridLines(g);
-                    drawHeadings(g);
-                    drawGrid(g);
-                    if (master.mode == 1) {
-                        master.drawGuesses(g);
-                    } else {
-                        master.drawDominoes(g);
-                    }
+            // Checks if the master object and its mode are valid
+            if (master != null && (master.mode == 1 || master.mode == 0)) {
+                l.drawGridLines(g);
+                drawHeadings(g);
+                drawGrid(g);
+                if (master.mode == 1) {
+                    master.drawGuesses(g);
+                } else {
+                    master.drawDominoes(g);
                 }
             }
         }
 
+        // Draws the background of the panel
         private void drawBackground(Graphics g) {
             g.setColor(Color.YELLOW);
             g.fillRect(0, 0, getWidth(), getHeight());
         }
 
+        // Draws the row and column headings
         private void drawHeadings(Graphics g) {
-            for (int are = 0; are < 7; are++) {
-                fillDigitGivenCentre(g, 10, 30 + are * 20, 20, are + 1);
+            for (int row = 0; row < 7; row++) {
+                fillDigitGivenCentre(g, 10, 30 + row * 20, 20, row + 1);
             }
-            for (int see = 0; see < 8; see++) {
-                fillDigitGivenCentre(g, 30 + see * 20, 10, 20, see + 1);
+            for (int col = 0; col < 8; col++) {
+                fillDigitGivenCentre(g, 30 + col * 20, 10, 20, col + 1);
             }
         }
 
+        // Draws the grid based on the master's grid data
         private void drawGrid(Graphics g) {
-            for (int are = 0; are < 7; are++) {
-                for (int see = 0; see < 8; see++) {
-                    drawDigitGivenCentre(g, 30 + see * 20, 30 + are * 20, 20, master.grid[are][see]);
+            for (int row = 0; row < 7; row++) {
+                for (int col = 0; col < 8; col++) {
+                    drawDigitGivenCentre(g, 30 + col * 20, 30 + row * 20, 20, master.grid[row][col]);
                 }
             }
         }
 
+        // Draws a digit at the specified position
         private void drawDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
             int radius = diameter / 2;
             g.setColor(Color.BLACK);
@@ -60,6 +65,7 @@ public class PictureFrame {
             g.drawString(txt, x - fm.stringWidth(txt) / 2, y + fm.getMaxAscent() / 2);
         }
 
+        // Fills a circle with a digit at the specified position
         private void fillDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
             int radius = diameter / 2;
             g.setColor(Color.GREEN);
@@ -72,6 +78,7 @@ public class PictureFrame {
         }
     }
 
+    // Constructor for the PictureFrame class
     public PictureFrame(Main sf) {
         master = sf;
         JFrame f = new JFrame("Abominodo");
@@ -82,6 +89,7 @@ public class PictureFrame {
         f.setVisible(true);
     }
 
+    // Method for resetting the frame, to be implemented if needed
     public void reset() {
         // TODO: Implement reset logic if needed
     }
