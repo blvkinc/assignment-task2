edb90dacab0a084899e2f24f473c5048d5fbb0a0
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-25 22:43:31 -0800

    added meaningful comments

--- base/IOSpecialist.java
+++ base/IOSpecialist.java
@@ -1,15 +1,21 @@
 package base;
 
 /**
+ * Represents a specialist for input/output operations.
+ * 
  * @author Kevan Buckley, maintained by __student
  * @version 2.0, 2014
  */
-
 public class IOSpecialist {
-	public IOSpecialist() {
-	}
+    public IOSpecialist() {
+    }
 
-	public String getString() {
-		return IOLibrary.getString();
-	}
+    /**
+     * Gets a string using the IOLibrary utility.
+     * 
+     * @return The string obtained from the IOLibrary
+     */
+    public String getString() {
+        return IOLibrary.getString();
+    }
 }
